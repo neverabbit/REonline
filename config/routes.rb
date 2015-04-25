1dde9b86753b5d82173df16c28a4b2e2c9db8b7f
neverabbit <neverrabbit@163.com>
2015-04-25 16:42:56 +0800

    Restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  root 'home_page#home'
+  # get 'user_interface/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
